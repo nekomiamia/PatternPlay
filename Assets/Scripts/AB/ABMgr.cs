fe016d734b2b9dc3bca0ed8cd3536f6fae9f15de
nekomiamia <407520408@qq.com>
2025-06-20 22:04:54 +0800

    feat: AB包管理器单例同步加载部分/测试代码

---
+++ Assets/Scripts/AB/ABMgr.cs
@@ -0,0 +1,134 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ABMgr: SingletonMono<ABMgr>
+{
+
+    // 主包
+    private AssetBundle mainAB = null;
+    // 配置信息 依赖信息
+    private AssetBundleManifest manifest = null;
+    private Dictionary<string, AssetBundle> abDic = new Dictionary<string, AssetBundle>();
+
+    /// <summary>
+    /// AB包存放路径
+    /// </summary>
+    private string PathURL
+    {
+        get
+        {
+            return Application.streamingAssetsPath + "/";
+        }
+    }
+    
+    /// <summary>
+    /// 主包名称
+    /// </summary>
+    private string MainABName
+    {
+        get
+        {
+#if UNITY_IOS
+            return "IOS";
+#elif UNITY_ANDROID
+            return "Android";
+#else
+            return "PC";
+#endif
+        }
+    }
+
+    /// <summary>
+    /// 加载AB包
+    /// </summary>
+    /// <param name="abName"></param>
+    public void LoadAB(string abName)
+    {
+        // 加载主包
+        if (mainAB == null)
+        {
+            mainAB = AssetBundle.LoadFromFile(PathURL + MainABName);
+            manifest = mainAB.LoadAsset<AssetBundleManifest>("AssetBundleManifest");
+        }
+        // 加载主包中的关键配置信息 获取依赖包
+        string[] strs = manifest.GetAllDependencies(abName);
+        
+        AssetBundle ab = null;
+        for (int i = 0; i < strs.Length; i++)
+        {
+            if (!abDic.ContainsKey(strs[i]))
+            {
+                ab = AssetBundle.LoadFromFile(PathURL + strs[i]);
+                abDic.Add(strs[i], ab);
+            }
+        }
+        
+        // 加载目标包
+        if (!abDic.ContainsKey(abName))
+        {
+            ab = AssetBundle.LoadFromFile(PathURL + abName);
+            abDic.Add(abName, ab);
+        }
+    }
+    
+    // 同步加载，不指定类型
+    public Object LoadRes(string abName, string resName)
+    {
+        LoadAB(abName);
+
+        Object obj = abDic[abName].LoadAsset(resName);
+        if (obj is GameObject)
+            return Instantiate(obj);
+        else
+            return obj;
+    }
+
+    // 同步加载，指定类型
+    public Object LoadRes(string abName, string resName, System.Type type)
+    {
+        LoadAB(abName);
+
+        Object obj = abDic[abName].LoadAsset(resName, type);
+        if (obj is GameObject)
+            return Instantiate(obj);
+        else
+            return obj;
+    }
+
+    // 同步加载，泛型
+    public T LoadRes<T>(string abName, string resName) where T : Object
+    {
+        LoadAB(abName);
+
+        T obj = abDic[abName].LoadAsset<T>(resName);
+        if (obj is GameObject)
+            return Instantiate(obj);
+        else
+            return obj;
+    }
+    
+    
+    // 异步加载
+    
+    
+    // 单个包卸载
+    public void UnLoad(string abName)
+    {
+        if (abDic.ContainsKey(abName))
+        {
+            abDic[abName].Unload(false);
+            abDic.Remove(abName);
+        }
+    }
+    
+    
+    // 卸载所有包
+    public void ClearAB()
+    {
+        AssetBundle.UnloadAllAssetBundles(false);
+        abDic.Clear();
+        mainAB = null;
+        manifest = null;
+    }
+}
